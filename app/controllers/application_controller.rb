c174fe920c5954ba77476d2e838a82a4bd118093
Paola Garcia <pacri14@gmail.com>
2021-10-13 15:42:38 +0000

    Adds Order details to the show action including logic to show order line items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
+  def order_subtotal_cents(order_items)
+    order_items.map {|entry| entry[:product].price_cents * entry[:line_item].quantity}.sum
+  end
+  helper_method :order_subtotal_cents 
 
   def update_cart(new_cart)
     cookies[:cart] = {
