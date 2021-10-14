0d5a5c2d3445d82a96d92e3dbfad6d2defe7d4b9
Paola Garcia <pacri14@gmail.com>
2021-10-14 20:56:17 +0000

    Adds list of categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,9 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+ 
+  def new
+    @category = Category.new
+  end
+end
